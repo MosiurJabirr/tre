2071fc3def4266e48193b5190344654a17bd1cab
MosiurJabirr <mosiur.jabir75@qmail.cuny.edu>
2023-07-21 09:36:19 -0400

    .

--- packages/Card/Card.tsx
+++ packages/Card/Card.tsx
@@ -1,14 +1,16 @@
+import React from 'react'
 import { StyledCard } from './Card.styled';
-
 type TCard = {
-  children?: React.ReactDOM;
+  children?: JSX.Element|JSX.Element[];
   bgColor?: string;
 }
 
 export const Card: React.FC<TCard> = ({ children, bgColor }) => {
   return (
+    
     <StyledCard bgColor={bgColor}>
       {children}
     </StyledCard>
+    
   );
 }
